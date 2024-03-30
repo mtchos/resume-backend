64d67f6c71c2133a70c873cc4fc90ec51ffcf40a
mtchos <matheussilvaesousa@gmail.com>
2024-03-30 15:45:29 -0300

    fix: clean imports

--- src/main/java/com/matheus/resumebuilder/model/Resume.java
+++ src/main/java/com/matheus/resumebuilder/model/Resume.java
@@ -1,10 +1,7 @@
 package com.matheus.resumebuilder.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
