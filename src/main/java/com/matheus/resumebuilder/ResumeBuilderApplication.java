5a66b066577d03366581057b362d118192d82286
mtchos <matheussilvaesousa@gmail.com>
2024-04-23 02:38:22 -0300

    fix: cors

--- src/main/java/com/matheus/resumebuilder/ResumeBuilderApplication.java
+++ src/main/java/com/matheus/resumebuilder/ResumeBuilderApplication.java
@@ -2,6 +2,10 @@ package com.matheus.resumebuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ResumeBuilderApplication {
@@ -10,4 +14,13 @@ public class ResumeBuilderApplication {
         SpringApplication.run(ResumeBuilderApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**");
+            }
+        };
+    }
 }
