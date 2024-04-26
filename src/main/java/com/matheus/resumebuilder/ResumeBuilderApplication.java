3c36906b8715b72d90b2bc1517622d34295039bb
mtchos <matheussilvaesousa@gmail.com>
2024-04-25 23:32:56 -0300

    fix: allow CORS from all origins (temporary)

--- src/main/java/com/matheus/resumebuilder/ResumeBuilderApplication.java
+++ src/main/java/com/matheus/resumebuilder/ResumeBuilderApplication.java
@@ -13,14 +13,4 @@ public class ResumeBuilderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResumeBuilderApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("**");
-            }
-        };
-    }
 }
