1f6746db5a186a68aa5ba083c5c273d76680c811
mtchos <matheussilvaesousa@gmail.com>
2024-04-26 22:38:54 -0300

    feat: rename cors configuration

--- src/main/java/com/matheus/resumebuilder/configuration/WebConfiguration.java
+++ src/main/java/com/matheus/resumebuilder/configuration/WebConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
