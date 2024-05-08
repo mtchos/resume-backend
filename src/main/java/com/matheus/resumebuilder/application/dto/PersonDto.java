33a01c485adab62ea5e349529ee8ee8fc61464ee
mtchos <matheussilvaesousa@gmail.com>
2024-05-08 01:39:08 -0300

    fix: todo - fix PersonDto.java

--- src/main/java/com/matheus/resumebuilder/application/dto/PersonDto.java
+++ src/main/java/com/matheus/resumebuilder/application/dto/PersonDto.java
@@ -1,19 +1,20 @@
-package com.matheus.resumebuilder.application.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.matheus.resumebuilder.business.entity.Resume;
-import lombok.Data;
-
-import java.util.List;
-import java.util.UUID;
-
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class PersonDto {
-
-    private UUID id;
-
-    private List<Resume> resumes;
-
-    private String username;
-}
+//package com.matheus.resumebuilder.application.dto;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import com.matheus.resumebuilder.business.entity.Resume;
+//import lombok.Data;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Data
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class PersonDto {
+//
+//    private UUID id;
+//
+//    private List<Resume> resumes;
+//
+//    private String username;
+//}
+// todo fix this dto
